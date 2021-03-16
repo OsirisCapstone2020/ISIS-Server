fb463f6ee563a1e6554034194a8b2ba436d47c95
mmi22 <43051795+mmi22@users.noreply.github.com>
2021-03-15 19:34:09 -0700

    Added ctxcal.py file to routes and updated app.py
    Created a new file ctxcal.py under file name routes
    
    In app.py file I updated the route for ctxcal

--- isis_server/app.py
+++ isis_server/app.py
@@ -1,58 +1,47 @@
-from tempfile import tempdir
+# Load the environment other code is called
+from .xml_reader import XMLReader
+from os import path, listdir
+from .s3 import S3Client
+from .routes.email import post_email
+from .routes import post_start, post_spiceinit, get_all_commands, get_command, \
+    post_mro_ctx_2_isis, get_output_file
+from flask import Flask
+from dotenv import load_dotenv
+load_dotenv()
 
-from flask import Flask, jsonify, request
-from .s3 import s3_download, s3_upload
-from .isis import lowpass, CMD_LOWPASS
-from .validator import REQUEST_SCHEMA
-from werkzeug.exceptions import BadRequest
-from os import path, remove as file_remove
-from flask_expects_json import expects_json
 
 STATUS_SERVER_ERROR = 500
 STATUS_BAD_REQUEST = 400
+XML_LOCATION = path.relpath(path.join(path.dirname(__file__), "..", "xml"))
 
-TEST_BUCKET = "test"
 
-# Create the APP
+def get_command_xml():
+    """
+    :return: A list of all the XML file in the data directory
+    """
+    all_files = [path.join(XML_LOCATION, f) for f in listdir(XML_LOCATION)]
+    return [XMLReader.get_isis_command(f) for f in all_files]
+
+
+# Create the app
 app = Flask(__name__)
+app.s3_client = S3Client()
+app.isis_commands = get_command_xml()
 
 # ===========
 # App routes
 # ===========
 
+# Add xml routes
+app.add_url_rule('/commands/<command_name>', view_func=get_command, methods=["GET"])
+app.add_url_rule('/commands', view_func=get_all_commands, methods=["GET"])
 
-@app.route("/", methods=["POST"])
-@expects_json(REQUEST_SCHEMA)
-def post_index():
-    """
-    Called when a user POSTs to /
-    """
-    input_data = request.json
+# Add n8n node routes
+app.add_url_rule('/start', view_func=post_start, methods=["POST"])
+app.add_url_rule('/email', view_func=post_email, methods=["POST"])
+app.add_url_rule('/spiceinit', view_func=post_spiceinit, methods=["POST"])
+app.add_url_rule('/mroctx2isis', view_func=post_mro_ctx_2_isis, methods=["POST"])
+app.add_url_rule('/ctxcal', view_func=post_ctx_cal, methods=["POST"])
 
-    # Download input
-    temp_in_file = s3_download(TEST_BUCKET, input_data["input_file"])
-
-    in_file_name, in_file_ext = path.splitext(input_data["input_file"])
-    out_obj_name = "{}.lowpass{}".format(in_file_name, in_file_ext)
-    out_obj_full_path = path.join(
-        tempdir,
-        out_obj_name
-    )
-
-    # Run ISIS
-    # TODO: Better design than if statements
-    if input_data["cmd"] == CMD_LOWPASS:
-        lowpass(temp_in_file, out_obj_full_path)
-    else:
-        raise BadRequest("Command not found")
-
-    # Upload output
-    upload_res = s3_upload(TEST_BUCKET, out_obj_full_path)
-
-    if path.exists(temp_in_file):
-        file_remove(temp_in_file)
-
-    if path.exists(out_obj_full_path):
-        file_remove(out_obj_full_path)
-
-    return jsonify({"output": out_obj_name})
+# Add the output route, which will serve output files
+app.add_url_rule('/output/<file_name>', view_func=get_output_file, methods=["GET"])
