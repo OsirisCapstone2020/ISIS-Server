2186215eb369c4ccee2a40b2d163f8e9e2a83e18
skh258 <61439962+skh258@users.noreply.github.com>
2021-03-16 14:00:08 +0000

    Added additional command functions  (#6)
    * Added cam2map command
    
    * Added ctxcal command
    
    * Added ctxevenodd command
    
    * Added isis2std command
    
    * Updated init file

--- isis_server/routes/__init__.py
+++ isis_server/routes/__init__.py
@@ -1,6 +1,10 @@
 from .spiceinit import post_spiceinit
 from .start import post_start
 from .mroctx2isis import post_mro_ctx_2_isis
+from .ctxcal import post_ctx_cal
+from .ctxevenodd import post_ctx_even_odd
+from .cam2map import post_cam_2_map
+from .isis2std import post_isis_2_std
 
 from .xml import get_all_commands, get_command
 from .output import get_output_file
